a1bfcffa293ad22f8920caaad3651546d936df73
Sebastian Nevarez <snevarez@bu.edu>
2020-03-27 19:21:51 -0400

    working on second page of web app

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/<handle>")
+def api_call(handle):
+    resp = handle
+    return resp
+
 
 if __name__ == "__main__":
     app.run(debug=True)
