f6914120155b73ec582c0b9ec77d154e86271ee4
Sebastian Nevarez <snevarez@bu.edu>
2020-03-27 23:17:05 -0400

    can make api call with a twitter handle

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
 
@@ -8,9 +9,11 @@ def index():
 
 @app.route("/<handle>")
 def api_call(handle):
-    resp = handle
-    return resp
-
+    api = "http://3.81.205.114/" + str(handle)
+    resp = requests.get(api) #call the api with the twitter handle
+    print(resp.status_code)
+    done = "done" #once video has been created load video
+    return done
 
 if __name__ == "__main__":
     app.run(debug=True)
