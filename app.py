b1d02820a967d5a224ccfbed97b03f80c8a59761
Sebastian Nevarez <snevarez@bu.edu>
2020-04-05 23:30:21 -0400

    finished python web app to test api

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, url_for
-import requests
+from flask import Flask, redirect, render_template, url_for, request, send_file
+from bs4 import BeautifulSoup
+import requests as req
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
-
-@app.route("/<handle>")
-def api_call(handle):
-    api = "http://3.81.205.114/" + str(handle)
-    resp = requests.get(api) #call the api with the twitter handle
-    print(resp.status_code)
-    done = "done" #once video has been created load video
-    return done
+    if request.method == "POST":
+        myHandle = request.form["myHandle"]
+        api = "http://54.152.189.151/" + str(myHandle)
+        resp = req.get(api, stream=True)
+        file_name = str(myHandle) + "_twitter_feed.mp4"
+        with open(file_name, 'wb') as f:
+            for chunk in resp.iter_content(chunk_size=1024*1024):
+                if chunk:
+                    f.write(chunk)
+        return send_file(file_name)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
